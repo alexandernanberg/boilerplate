04fb0dc8a4eec27fe770b17d29c3cfbfc70a1fbc
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-04-24 11:54:07 +0200

    Move glyphs to separate folder and add propTypes to Icon comonent

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import style from './style.scss'
 
-const files = require.context('.', false, /.svg$/)
+const files = require.context('./glyphs', false, /.svg$/)
 files.keys().forEach(files)
 
 const Icon = ({ name, ...props }) =>
   <svg {...props} className={classNames(style.component, props.className)}>
     <use xlinkHref={`#${name}`} />
   </svg>
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+}
 
 export default Icon
