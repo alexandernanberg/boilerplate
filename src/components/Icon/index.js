f3f0f7eba8cd5d264f641194dda8730f9807006e
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-04-24 11:55:31 +0200

    Place proptypes import directly under react

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import classNames from 'classnames'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import style from './style.scss'
 
 const files = require.context('./glyphs', false, /.svg$/)
