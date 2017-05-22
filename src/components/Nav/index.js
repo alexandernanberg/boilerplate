0aaa493ace3457509b2c71bfc161828e80bc7bfa
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-05-22 08:32:49 +0200

    Nicer code format

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,8 +4,19 @@ import style from './style.scss'
 
 const Nav = () => (
   <nav className={style.component}>
-    <NavLink exact to="/" className={style.link} activeClassName={style.linkActive}>Home</NavLink>
-    <NavLink to="/about" className={style.link} activeClassName={style.linkActive}>About</NavLink>
+    <NavLink
+      exact
+      to="/"
+      className={style.link}
+      activeClassName={style.linkActive}>
+      Home
+    </NavLink>
+    <NavLink
+      to="/about"
+      className={style.link}
+      activeClassName={style.linkActive}>
+      About
+    </NavLink>
   </nav>
 )
 
