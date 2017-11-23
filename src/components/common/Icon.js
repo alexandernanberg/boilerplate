49414c70c0efde4f6b5f57a779999811a1b1546f
Alexander Nanberg <alex.nanberg@hotmail.com>
2017-11-23 17:48:55 +0100

    Inline preval icon map

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import iconMap from '../../utils/iconMap'
+
+const iconMap = preval`
+  const fs = require('fs')
+  const path = require('path')
+
+  const iconPath = path.resolve('public/static/icons')
+  const icons = fs.readdirSync(iconPath)
+
+  module.exports = icons
+    .reduce((acc, file) => {
+      const content = fs.readFileSync(path.join(iconPath, file), 'utf8')
+
+      acc[file.slice(0, -4)] = {
+        path: /d="(.*?)"/.exec(content)[1],
+        viewBox: /viewBox="(.*?)"/.exec(content)[1],
+      }
+      return acc
+    }, {})
+`
 
 const Svg = styled.svg`
   display: inline-block;
