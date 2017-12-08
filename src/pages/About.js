45a84ec2ef7e110c8df5d4e508c7ce8c695a8726
Alexander Nanberg <alex.nanberg@hotmail.com>
2017-12-08 13:14:01 +0100

    Add meta description

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Page from '../components/Page'
 import { H1 } from '../components/common/Title'
 
 export default () => (
-  <Page title="About">
+  <Page title="About" meta={[{ name: 'description', content: 'About us page' }]}>
     <H1>About.</H1>
   </Page>
 )
