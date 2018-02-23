5a5469b90d621ac92f0b4ca7b7ff1752be8db5f5
Alexander Nanberg <alex.nanberg@hotmail.com>
2018-02-23 21:52:43 +0100

    Add service-worker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDom from 'react-dom'
 import App from './components/App'
 
 ReactDom.render(<App />, document.getElementById('root'))
+
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+    })
+  }
+}
