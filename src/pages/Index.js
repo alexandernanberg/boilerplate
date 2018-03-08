261e9b4d9cd840be23ce0ceb06bfaed3041371c9
Alexander <alex.nanberg@hotmail.com>
2018-03-08 20:00:57 +0100

    Updates

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Button } from '@strt/ui'
 import Page from '../components/Page'
 import { H1 } from '../components/common/Title'
 
 export default () => (
   <Page>
     <H1>Home.</H1>
-    <Button>ButtON</Button>
   </Page>
 )
