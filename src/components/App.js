53dd38bce782723067f83eee7be81394d47a525b
Alexander <alex.nanberg@hotmail.com>
2018-04-04 20:01:09 +0200

    Lazy load all routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 import { injectGlobalStyle } from '../style'
 import Header from './Header'
-import Index from '../pages/Index'
-import About from '../pages/About'
-import NotFound from '../pages/NotFound'
+
+const Loader = () => null
+
+const Index = Loadable({
+  loader: () => import('../pages/Index' /* webpackChunkName: "index.route" */),
+  loading: Loader,
+})
+
+const About = Loadable({
+  loader: () => import('../pages/About' /* webpackChunkName: "about.route" */),
+  loading: Loader,
+})
+
+const NotFound = Loadable({
+  loader: () => import('../pages/NotFound' /* webpackChunkName: "404.route" */),
+  loading: Loader,
+})
 
 const App = () => {
   injectGlobalStyle()
@@ -20,9 +35,11 @@ const App = () => {
         />
         <Header />
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
         </Switch>
       </Fragment>
     </Router>
