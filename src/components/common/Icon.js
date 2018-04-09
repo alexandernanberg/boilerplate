5dcc6bb523fb01ad88bcf504c2e16b6575a64ced
Alexander <alex.nanberg@hotmail.com>
2018-04-09 21:57:47 +0200

    Use preval as babel macro

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import preval from 'preval.macro'
 
 const iconMap = preval`
   const fs = require('fs')
